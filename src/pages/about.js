d69707862b9ea452272e477f6f9cc31633983784
Carie Fisher <carie.fisher@deque.com>
2019-06-04 13:08:58 -0500

    more content updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -23,26 +23,25 @@ const AboutPage = ({ data }, location) => {
         <div className="post-content-body about">
           <Icon />
           <p>
-            I have been building websites professionally since the early 2000s
-            and currently working as a Senior Accessibility Consultant at{" "}
-            <a href="https://www.deque.com/">Deque Systems</a>. I am passionate
+            I have been building websites professionally since the early 2000s and I am passionate
             about front-end development, accessibility, and promoting diversity
-            in the tech world. I am the creator of the{" "}
-            <a href="https://a11y-style-guide.com/style-guide/">
+            in the tech world. Currently, I am working as a Senior Accessibility Consultant at{" "}
+            <a href="https://www.deque.com/" rel="noopener noreferrer" target="_blank">Deque Systems</a>.</p><p>When I am not spending time with my family, traveling, gardening, or making a mess with my latest DIY project, I volunteer my time to the accessibility and dev communities. I am the creator of the{" "}
+            <a href="https://a11y-style-guide.com/style-guide/" rel="noopener noreferrer" target="_blank">
               Accessibility (A11Y) Style Guide
             </a>{" "}
             and{" "}
-            <a href="https://www.youtube.com/c/accessibilitytalks">
+            <a href="https://www.youtube.com/c/accessibilitytalks" rel="noopener noreferrer" target="_blank">
               Accessibility (A11Y) Talks
             </a>{" "}
             virtual meet-up, designer of the{" "}
-            <a href="https://www.bonfire.com/accessibility-cats/">
+            <a href="https://www.bonfire.com/accessibility-cats/" rel="noopener noreferrer" target="_blank">
               A11y Cats T-shirt
             </a>
-            , member of the <a href="https://a11yproject.com/">A11y Project</a>{" "}
-            and <a href="https://www.24a11y.com/">24a11y</a> teams, and past
+            , member of the <a href="https://a11yproject.com/" rel="noopener noreferrer" target="_blank">A11y Project</a>{" "}
+            and <a href="https://www.24a11y.com/" rel="noopener noreferrer" target="_blank">24a11y</a> teams, and past
             team member and mentor for the{" "}
-            <a href="https://air-rallies.org/">OpenAIR Knowbility Challenge</a>.
+            <a href="https://air-rallies.org/" rel="noopener noreferrer" target="_blank">OpenAIR Knowbility Challenge</a>.
           </p>
         </div>
       </article>
--- src/pages/speak.js
+++ src/pages/speak.js
@@ -21,7 +21,7 @@ const AboutPage = ({ data }, location) => {
       <article className="post-content page-template no-image">
         <div className="post-content-body speak">
           <Icon />
-          <p>When I have a great idea, I can't keep it to myself!</p><p>Between client trainings and conference talks, I love to share some of the knowledge I've gained these past 15 years or so in the tech business. I am always looking for opportunities to speak - especially in tropical locations in winter (it's cold in Wisconsin)!</p><p>Here are some of the places I've been fortunate enough to speak at:<ul><li>a11yTO</li><li>Deque Systems (client trainings)</li><li>DrupalCon</li><li>various Drupal camps</li><li>WordSesh</li><li>WordPress Campus</li></ul></p><p>For more info, see my <a href="https://noti.st/cariefisher" rel="noopener noreferrer" target="_blank">speaking portfolio on Noti.st</a>.</p>
+          <p>When I have a great idea, I can't keep it to myself!</p><p>Between client trainings and conference talks, I love to share some of the knowledge I've gained these past 15 years or so in the tech business. I am always looking for opportunities to speak - especially in tropical locations in winter (it's cold in Wisconsin)!</p><p>Here are some of the places I've been fortunate enough to speak at:<ul><li>a11yTO</li><li>Deque Systems (client trainings)</li><li>DrupalCon</li><li>various Drupal camps</li><li>WordSesh</li><li>WordPress Campus</li></ul></p><p>For more info, see my <a href="https://noti.st/cariefisher" rel="noopener noreferrer" target="_blank">speaking portfolio on Noti.st</a></p>
         </div>
       </article>
     </Layout>
