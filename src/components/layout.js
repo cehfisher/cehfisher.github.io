5bcdf06958b7296fa5aa5fd1672559b4f8a670de
Carie Fisher <carie.fisher@deque.com>
2019-06-05 23:35:21 -0500

    svgs

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,18 @@
 /* eslint-disable */
 import React from "react"
 import { Link } from "gatsby"
-import Icon from "../../content/assets/wave.svg";
+import Icon1 from "../../content/assets/home.svg";
+import Icon2 from "../../content/assets/mic.svg";
+import Icon3 from "../../content/assets/pen.svg";
+import { FiHome } from 'react-icons/fi';
+import { FiTwitter } from 'react-icons/fi';
+import { FiLinkedin } from 'react-icons/fi';
+import { FiSmile } from 'react-icons/fi';
+import { FiPenTool } from 'react-icons/fi';
+import { FiGithub } from 'react-icons/fi';
+import { FiStar } from 'react-icons/fi';
+import { FiMic } from 'react-icons/fi';
+import { FiHeart } from 'react-icons/fi';
 
 class Layout extends React.Component {
   render() {
@@ -11,57 +22,26 @@ class Layout extends React.Component {
       <div className="site-wrapper">
         <header className="site-head">
           <div className="site-head-container">
-            <nav id="swup" class="site-head-left">
-              <ul className="nav" role="menu">
-                <li className="nav-home nav-current" role="menuitem">
-                  <Link to={`/`}>Home</Link>
-                </li>
-                <li className="nav-about" role="menuitem">
-                  <Link to={`/about`}>About</Link>
-                </li>
-                <li className="nav-speak" role="menuitem">
-                  <Link to={`/speak`}>Speak</Link>
-                </li>
-                <li className="nav-write" role="menuitem">
-                  <Link to={`/write`}>Write</Link>
-                </li>
-              </ul>
-            </nav>
             <div className="site-head-right">
-              <div className="social-links">
-                <a
-                  href="https://twitter.com/cariefisher"
-                  title="Twitter"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Twitter
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/cariefisher"
-                  title="LinkedIn"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-                <a
-                  href="https://noti.st/cariefisher"
-                  title="Noti.st"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Noti.st
-                </a>
-                <a
-                  href="https://www.github.com/cehfisher"
-                  title="GitHub"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>
-              </div>
+              <nav id="swup">
+                <ul className="nav" role="menu">
+                  <li className="nav-home" role="menuitem">
+                    <Link to={`/`}><Icon1 /><span class="hidden">Home</span></Link>
+                  </li>
+                  <li className="nav-about" role="menuitem">
+                    <Link to={`/about`}><FiSmile /><span class="hidden">About</span></Link>
+                  </li>
+                  <li className="nav-speak" role="menuitem">
+                    <Link to={`/speak`}><Icon2 /><span class="hidden">Speak</span></Link>
+                  </li>
+                  <li className="nav-write" role="menuitem">
+                    <Link to={`/write`}><Icon3 /><span class="hidden">Write</span></Link>
+                  </li>
+                  <li className="nav-accessibility" role="menuitem">
+                    <Link to={`/accessibility`}><FiHeart /><span class="hidden">Accessibility</span></Link>
+                  </li>
+                </ul>
+              </nav>
             </div>
           </div>
         </header>
@@ -71,7 +51,41 @@ class Layout extends React.Component {
           </div>
         </main>
         <footer className="site-foot">
-          &copy; {new Date().getFullYear()} {title}{" "} - <Link to={`/accessibility`}>Accessibility Statement</Link>
+          <ul className="nav" role="menu">
+            <li><a
+              href="https://twitter.com/cariefisher"
+              title="Twitter"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiTwitter /><span class="hidden">Twitter</span>
+            </a></li>
+            <li><a
+              href="https://www.linkedin.com/in/cariefisher"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiLinkedin /><span class="hidden">LinkedIn</span>
+            </a></li>
+            <li><a
+              href="https://noti.st/cariefisher"
+              title="Noti.st"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiStar /><span class="hidden">Notist</span>
+            </a></li>
+            <li><a
+              href="https://www.github.com/cehfisher"
+              title="GitHub"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiGithub /><span class="hidden">GitHiub</span>
+            </a></li>
+          </ul>
+          <p>&copy; {new Date().getFullYear()} {title}{" "}</p>
         </footer>
       </div>
     )
