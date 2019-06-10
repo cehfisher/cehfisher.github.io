f1624d254c84098d6b30fb0ad694559f7a87b516
Carie Fisher <carie.fisher@deque.com>
2019-06-10 12:59:36 -0500

    more styles for menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,17 +14,140 @@ import { FiGithub } from 'react-icons/fi';
 import { FiStar } from 'react-icons/fi';
 import { FiMic } from 'react-icons/fi';
 import { FiHeart } from 'react-icons/fi';
+import Particles from 'react-particles-js';
+import $ from 'jquery';
 
 class Layout extends React.Component {
+  componentDidMount () {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 10) {
+        $('.main-menu').addClass('floatingHeader');
+      } else {
+        $('.main-menu').removeClass('floatingHeader');
+      }
+    })
+  }
+
   render() {
     const { title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
     return (
       <div className="site-wrapper">
+        <Particles className="snow"
+    params={{
+          "particles": {
+            "number": {
+              "value": 80,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#8323fa"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000"
+              },
+              "polygon": {
+                "nb_sides": 10
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.3,
+              "random": false,
+              "anim": {
+                "enable": true,
+                "speed": 0.5,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 10,
+              "random": true,
+              "anim": {
+                "enable": true,
+                "speed": 0,
+                "size_min": 0,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": false,
+              "distance": 200,
+              "color": "#ffffff",
+              "opacity": 1,
+              "width": 2
+            },
+            "move": {
+              "enable": true,
+              "speed": 1,
+              "direction": "none",
+              "random": true,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": true,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": false,
+                "mode": "repulse"
+              },
+              "onclick": {
+                "enable": false,
+                "mode": "push"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 200,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+    	}} />
         <header className="site-head">
           <div className="site-head-container">
             <div className="site-head-right">
-              <nav id="swup">
+              <nav id="swup" className="main-menu">
                 <ul className="nav" role="menu">
                   <li className="nav-home" role="menuitem">
                     <Link to={`/`} aria-label="Home"><Icon1 /><span className="hidden">Home</span></Link>
