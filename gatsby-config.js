1681d863ec6b13506007d6872399f4574af11ffb
Carie Fisher <carie.fisher@deque.com>
2019-06-13 14:00:43 -0500

    content updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,28 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-accessibilityjs`,
+      options: {
+        injectStyles: `
+          .accessibility-error {
+            box-shadow: 0 0 3px 1px #f00;
+            background-color: rgba(255, 0, 0, 0.25);
+            position: relative;
+          }
+          .accessibility-error:before {
+            content: "A11Y";
+            position: absolute;
+            top: 0;
+            left: 0;
+            color: #fff;
+            font-size: 10px;
+            background-color: rgba(255, 0, 0, 0.5);
+            transform: translateY(-100%);
+          `,
+        },
+      },
+    ];
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-141604839-1",
