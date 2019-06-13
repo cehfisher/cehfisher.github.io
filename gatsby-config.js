bdbf895e01ea9870b41f07675edca2fa212e4159
Carie Fisher <carie.fisher@deque.com>
2019-06-13 14:24:19 -0500

    more content

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,24 +16,23 @@ module.exports = {
       resolve: `gatsby-plugin-accessibilityjs`,
       options: {
         injectStyles: `
-          .accessibility-error {
-            box-shadow: 0 0 3px 1px #f00;
-            background-color: rgba(255, 0, 0, 0.25);
-            position: relative;
-          }
-          .accessibility-error:before {
-            content: "A11Y";
-            position: absolute;
-            top: 0;
-            left: 0;
-            color: #fff;
-            font-size: 10px;
-            background-color: rgba(255, 0, 0, 0.5);
-            transform: translateY(-100%);
-          `,
-        },
+        .accessibility-error {
+          box-shadow: 0 0 3px 1px #f00,
+          background-color: rgba(255, 0, 0, 0.25),
+          position: relative,
+        }
+        .accessibility-error:before {
+          content: "A11Y",
+          position: absolute,
+          top: 0,
+          left: 0,
+          color: #fff,
+          font-size: 10px,
+          background-color: rgba(255, 0, 0, 0.5),
+          transform: translateY(-100%),
+        `,
       },
-    ];
+    },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
