fd009e0fcd85cf4dcbf3bb737c3a116b78bd7fc3
Carie Fisher <carie.fisher@deque.com>
2019-06-17 22:19:00 -0500

    desc update

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Carie Fisher",
   shortName: "Carie Fisher",
   description:
-    "The more code I learn, the more I understand it is not about the code at all.",
+    "The more code I learn, the more I understand it's not about the code at all.",
   url: "https://cariefisher.com", // Domain of your site without prefix!
   prefix: "/",
   author: "Carie Fisher",
